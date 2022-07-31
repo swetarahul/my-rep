ac93dac46ed9e4787aae93dbcf6f7cc604c5f4b1
swetarahul <110330294+swetarahul@users.noreply.github.com>
2022-07-31 16:15:42 +0530

    1st

---
+++ Untitled2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+pd.read_csv("https://raw.githubusercontent.com/dsrscientist/dataset1/master/Skyserver.csv")
+
+
+# In[ ]:
+
+
+pd.read_csv()
+
